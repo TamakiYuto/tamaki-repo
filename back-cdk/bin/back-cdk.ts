93c99f4c46d73213033714897bfaccae3d1dc4b2
TamakiYuto <yuto.tamaki@g.dreamcareer.co.jp>
2024-07-10 15:46:54 +0900

    Lambda

--- back-cdk/bin/back-cdk.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { LambdaApiGatewayStack } from '../lib/back-cdk-stack';
-
-const app = new cdk.App();
-new LambdaApiGatewayStack(app, 'LambdaApiGatewayStack');
