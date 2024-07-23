ab3443319bb3a24602753d3e125273d307020567
TamakiYuto <yuto.tamaki@g.dreamcareer.co.jp>
2024-07-23 12:47:32 +0900

    RDSDelete

--- backend-cdk/lib/my-serverless-project-stack.ts
+++ backend-cdk/lib/my-serverless-project-stack.ts
@@ -15,34 +15,34 @@ export class MyServerlessProjectStack extends cdk.Stack {
     });
 
        // RDSインスタンスを作成 (MariaDB)
-       const rdsInstance = new rds.DatabaseInstance(this, 'MyRdsInstance', {
-        engine: rds.DatabaseInstanceEngine.mariaDb({ version: rds.MariaDbEngineVersion.VER_10_5 }),
-        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
-        vpc,
-        credentials: rds.Credentials.fromGeneratedSecret('admin'), // 管理者ユーザーの生成
-        databaseName: 'MyDatabase',
-        allocatedStorage: 20, // 20GBのストレージ
-        maxAllocatedStorage: 100, // 最大100GBまで拡張可能
-        vpcSubnets: {
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        removalPolicy: cdk.RemovalPolicy.DESTROY, // スタック削除時にRDSインスタンスも削除
-      });
+      //  const rdsInstance = new rds.DatabaseInstance(this, 'MyRdsInstance', {
+      //   engine: rds.DatabaseInstanceEngine.mariaDb({ version: rds.MariaDbEngineVersion.VER_10_5 }),
+      //   instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      //   vpc,
+      //   credentials: rds.Credentials.fromGeneratedSecret('admin'), // 管理者ユーザーの生成
+      //   databaseName: 'MyDatabase',
+      //   allocatedStorage: 20, // 20GBのストレージ
+      //   maxAllocatedStorage: 100, // 最大100GBまで拡張可能
+      //   vpcSubnets: {
+      //     subnetType: ec2.SubnetType.PUBLIC,
+      //   },
+      //   removalPolicy: cdk.RemovalPolicy.DESTROY, // スタック削除時にRDSインスタンスも削除
+      // });
 
      // Lambda関数を作成
      const myLambda = new lambda.Function(this, 'MyLambda', {
-      runtime: lambda.Runtime.NODEJS_20_X,
+      runtime: lambda.Runtime.NODEJS_10_X,
       handler: 'handlers/myLambda.handler',
       code: lambda.Code.fromAsset('src'),
-      environment: {
-        DATABASE_SECRET_ARN: rdsInstance.secret!.secretArn,
-        DATABASE_NAME: 'MyDatabase',
-      },
+      // environment: {
+      //   DATABASE_SECRET_ARN: rdsInstance.secret!.secretArn,
+      //   DATABASE_NAME: 'MyDatabase',
+      // },
       vpc,
     });
 
      // RDSインスタンスへの接続許可をLambdaに付与
-     rdsInstance.connections.allowDefaultPortFrom(myLambda);
+    //  rdsInstance.connections.allowDefaultPortFrom(myLambda);
 
     // API Gatewayを作成し、Lambda関数をエンドポイントとして設定
     new apigateway.LambdaRestApi(this, 'MyApi', {
